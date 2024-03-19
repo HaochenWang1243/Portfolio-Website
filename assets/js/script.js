3e8b1b286ec86b977d9d1d6eaf13901f171dfecc
HaochenWang1243 <haw057@ucsd.edu>
2024-03-19 00:00:29 -0700

    update bio

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Web Development", "Machine Learning", "C#/Unity Game Development"],
+    strings: ["Web Development", "Machine Learning", "Cloud Computing"], //C#/Unity Game Development
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
